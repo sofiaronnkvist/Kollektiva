a363b92daf8ab2596384c744517f7c3af8867a93
s0wie <sowieh@gmail.com>
2022-04-13 23:08:45 +0200

    more pages

--- pages/skapaannons/1.tsx
+++ pages/skapaannons/1.tsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import LoginHeader from '../../components/LoginHeader'
-import Link from 'next/link'
-import Image from 'next/image'
+import ButtonSm2 from '../../components/Buttons/ButtonSm2'
+import QuestionMark from '../../components/QuestionMark'
 
 function SkapaAnnons1() {
   return (
     <div>
       <LoginHeader />
-      <div className="container mx-auto mt-[72px] flex items-center justify-center bg-white">
+      <div className="container mx-auto mt-[50px] flex items-center justify-center bg-white">
         <div className="w-[548px]">
           <h1 className="mb-[24px] text-[36px] text-indigo-800">
             Vilket typ av boende vill du hyra ut?
           </h1>
+          <h2 className="text-[20px] text-indigo-800">
+            Vilket typ av bostad hyr du ut?
+          </h2>
+          <div className="my-[24px] flex justify-between">
+            <button className="choice-button">Villa</button>
+            <button className="choice-button">Lägenhet</button>
+            <button className="choice-button">Attefallshus</button>
+            <button className="choice-button">Stuga</button>
+          </div>
+          <h2 className="my-[24px] text-[20px] text-indigo-800">
+            Vill du hyra hela bostaden eller ett rum i bostaden?
+          </h2>
+          <div className="flex">
+            <button className="choice-button mr-6">Hela bostaden</button>
+            <button className="choice-button">Rum i bostaden</button>
+          </div>
+          <h2 className="my-[24px] text-[20px] text-indigo-800">
+            Hyr du ut möblerat?
+          </h2>
+          <div className="mb-[80px] flex">
+            <button className="choice-button mr-6">Möblerat</button>
+            <button className="choice-button">Omöblerat</button>
+          </div>
+          <div className="flex items-center justify-between">
+            <ButtonSm2 text="Tillbaka" />
+            <p className="text-[20px] text-indigo-700">1/9</p>
+            <button className="h-[52px] w-[185px] rounded-full bg-indigo-500 text-[24px] text-white">
+              Nästa
+            </button>
+          </div>
         </div>
+        <QuestionMark />
       </div>
     </div>
   )
---
+++ pages/skapaannons/2.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginHeader from '../../components/LoginHeader'
+
+function SkapaAnnons2() {
+  return (
+    <div>
+      <LoginHeader />
+    </div>
+  )
+}
+
+export default SkapaAnnons2
