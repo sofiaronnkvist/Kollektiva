a8a9cfef1c32aa49cb2ac02646a270c6d6e0bd17
s0wie <sowieh@gmail.com>
2022-04-13 21:45:56 +0200

    /blimedlem, /blimedlem/1 done

--- pages/blimedlem/1.tsx
+++ pages/blimedlem/1.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import ButtonLg from '../../components/Buttons/ButtonLg'
+import LoginHeader from '../../components/LoginHeader'
+import QuestionMark from '../../components/QuestionMark'
 
 function Blimedlem1() {
   return (
-    <div className="flex h-screen items-center bg-slate-50">
-      <div className="container mx-auto bg-white">
-        <h1 className="text-center">Bli en del av Kollektiva</h1>
-        <p>
+    <div>
+      <LoginHeader />
+      <div className="container mx-auto w-[450px] bg-white">
+        <h1 className="mt-[72px] text-[30px]">Bli en del av Kollektiva</h1>
+        <p className="text-[20px] text-indigo-800">
           Hos Kollektiva hör alla hemma. För att säkerställa det så ber vi dig
           att skriva under på följande:
         </p>
-        <p>
+        <p className="text-[20px]">
           Jag samtycker till att behandla alla på Kollektiva med respekt och
           utan fördomar, oavsett religion, nationellt ursprung, etnicitet,
           funktionsvariation, kön, könsidentitet, sexuell läggning eller ålder.
         </p>
         <ButtonLg text="Godkänn" />
       </div>
+      <QuestionMark />
     </div>
   )
 }
--- pages/blimedlem/index.tsx
+++ pages/blimedlem/index.tsx
@@ -1,36 +1,77 @@
 import React from 'react'
-import ButtonLg from '../../components/Buttons/ButtonLg'
-import ButtonSm from '../../components/Buttons/ButtonSm'
-import ButtonSm2 from '../../components/Buttons/ButtonSm2'
-import ButtonMd from '../../components/Buttons/ButtonMd'
 import LoginHeader from '../../components/LoginHeader'
+import Link from 'next/link'
+import QuestionMark from '../../components/QuestionMark'
 
 function Register() {
   return (
     <div>
       <LoginHeader />
-      <div className="flex h-screen items-center bg-slate-50">
-        <div className="container mx-auto h-[400px] bg-white">
-          <h1 className="text-center">Bli medlem</h1>
-          <form action="">
-            <label htmlFor="namn">Förnamn</label>
-            <input type="text" name="namn" id="namn" className="border" />
-            <label htmlFor="efternamn">Efternamn</label>
-            <input
-              type="text"
-              name="efternamn"
-              id="efternamn"
-              className="border"
-            />
-            <label htmlFor="mail">E-postadress</label>
-            <input type="text" name="mail" id="mail" className="border" />
-          </form>
-          <ButtonLg text="Godkänn och fortsätt" />
-          <ButtonSm text="Nästa" />
-          <ButtonMd text="Mina annonser" />
-          <ButtonSm2 text="Tillbaka" />
+      <div>
+        <div className="container mx-auto">
+          <h1 className="mt-[72px] text-center text-[30px]">
+            Slutför registreringen
+          </h1>
+          <div className="flex justify-center">
+            <form action="">
+              <label
+                className="mb-2 block text-sm text-gray-700"
+                htmlFor="namn"
+              >
+                Förnamn
+              </label>
+              <input
+                type="text"
+                name="namn"
+                id="namn"
+                placeholder="Namn"
+                className="focus:shadow-outline mb-2 w-[320px] appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              />
+              <label
+                className="mb-2 block text-sm text-gray-700"
+                htmlFor="efternamn"
+              >
+                Efternamn
+              </label>
+              <input
+                type="text"
+                name="efternamn"
+                id="efternamn"
+                placeholder="Efternamn"
+                className="focus:shadow-outline mb-2 w-[320px] appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              />
+              <p className="mb-[56px] text-xs text-gray-600">
+                Namnet ska matcha ditt nationella ID.
+              </p>
+              <label
+                className="mb-2 block text-sm text-gray-700"
+                htmlFor="mail"
+              >
+                E-postadress
+              </label>
+              <input
+                type="text"
+                name="mail"
+                id="mail"
+                placeholder="epost@epost.se"
+                className="focus:shadow-outline mb-2 w-[320px] appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              />
+              <p className="mb-[56px] text-xs text-gray-600">
+                Vi e-postar dig bekräftelser och kvitton.
+              </p>
+              <Link href="/blimedlem/1">
+                <button
+                  type="submit"
+                  className="h-[52px] w-[320px] rounded-full  bg-indigo-500 text-[20px] text-white"
+                >
+                  Godkänn och fortsätt
+                </button>
+              </Link>
+            </form>
+          </div>
         </div>
       </div>
+      <QuestionMark />
     </div>
   )
 }
