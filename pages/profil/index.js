ce2edd44917e9e6a928522a2796eedd5854adba9
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2022-04-20 14:11:35 +0200

    Profile index things.

--- pages/profil/index.js
+++ pages/profil/index.js
@@ -5,6 +5,32 @@ function Profil() {
   return (
     <div>
       <NavLoggedIn name="Marianne W" />
+      <div>
+          <div>
+              <img src="#" alt='Profilbild'/>
+              <h2>Marianne Wilson</h2>
+              <p>Linköping • Registrerad Maj 2021</p>
+              <button>Redigera profil</button>
+          </div>
+          <div className='flex flex-col'>
+            <div className='flex gap-2'>
+                <img src='/Spara.png' alt='Spara'/>
+                <button>Mina annonser</button>
+            </div>
+            <div className='flex gap-2'>
+                <img src='/Smiley.png' alt='Smiley' />
+                <button>Ansökningar</button>
+            </div>
+            <div className='flex gap-2'>
+                <img src='/Pratbubbla.png' alt='Pratbubbla'/>
+                <button>Konversationer</button>
+            </div>
+            <div className='flex gap-2'>
+                <img src='/Hjärta.png' alt='Hjärta' />
+                <button>Sparade annonser</button>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
