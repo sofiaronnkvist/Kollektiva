d053aa2ade00d52e98632a0eba246d8d6c5fcd2d
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2022-04-20 14:54:11 +0200

    Left menu bar profile.

--- pages/profil/index.js
+++ pages/profil/index.js
@@ -5,29 +5,29 @@ function Profil() {
   return (
     <div>
       <NavLoggedIn name="Marianne W" />
-      <div>
-          <div>
-              <img src="#" alt='Profilbild'/>
-              <h2>Marianne Wilson</h2>
-              <p>Linköping • Registrerad Maj 2021</p>
-              <button>Redigera profil</button>
+      <div className='p-20 flex flex-col gap-20'>
+          <div className='flex flex-col gap-4'>
+              <img className='rounded-full h-[86px] w-[86px]' src="/Marianne.png" alt='Profilbild'/>
+              <h2 className='text-3xl'>Marianne Wilson</h2>
+              <p className='text-base font-normal'>Linköping • Registrerad Maj 2021</p>
+              <button className="h-[29px] w-[136px] text-base rounded-full border border-indigo-700 text-[16px] text-indigo-900 font-medium">Redigera profil</button>
           </div>
-          <div className='flex flex-col'>
-            <div className='flex gap-2'>
-                <img src='/Spara.png' alt='Spara'/>
-                <button>Mina annonser</button>
+          <div className='flex flex-col gap-8'>
+            <div className='flex items-center gap-2 pl-4 rounded-full hover:bg-indigo-50 h-[52px] w-[250px]'>
+                <img className='h-[24px]' src='/Spara.png' alt='Spara'/>
+                <button className='text-gray-500 hover:text-indigo-500'>Mina annonser</button>
             </div>
-            <div className='flex gap-2'>
-                <img src='/Smiley.png' alt='Smiley' />
-                <button>Ansökningar</button>
+            <div className='flex items-center gap-2 pl-4 rounded-full hover:bg-indigo-50 h-[52px] w-[250px]'>
+                <img className='h-[24px]' src='/Smiley.png' alt='Smiley' />
+                <button className='text-gray-500 hover:text-indigo-500'>Ansökningar</button>
             </div>
-            <div className='flex gap-2'>
-                <img src='/Pratbubbla.png' alt='Pratbubbla'/>
-                <button>Konversationer</button>
+            <div className='flex items-center gap-2 pl-4 rounded-full hover:bg-indigo-50 h-[52px] w-[250px]'>
+                <img className='h-[24px]' src='/Pratbubbla.png' alt='Pratbubbla'/>
+                <button className='text-gray-500 hover:text-indigo-500'>Konversationer</button>
             </div>
-            <div className='flex gap-2'>
-                <img src='/Hjärta.png' alt='Hjärta' />
-                <button>Sparade annonser</button>
+            <div className='flex items-center gap-2 pl-4 rounded-full hover:bg-indigo-50 h-[52px] w-[250px]'>
+                <img className='h-[24px]' src='/Hjärta.png' alt='Hjärta' />
+                <button className='text-gray-500 hover:text-indigo-500'>Sparade annonser</button>
             </div>
           </div>
       </div>
