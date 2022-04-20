fbdef34fe8931108e0219900e18e8d68321628be
s0wie <sowieh@gmail.com>
2022-04-20 13:49:01 +0200

    Added navbar to /profil

---
+++ pages/profil/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavLoggedIn from '../../components/Navigations/NavLoggedIn'
+
+function Profil() {
+  return (
+    <div>
+      <NavLoggedIn />
+    </div>
+  )
+}
+
+export default Profil
