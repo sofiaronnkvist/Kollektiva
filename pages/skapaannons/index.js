a8e609cca9e7e0d681ac4650bc5743985f50d8a7
s0wie <sowieh@gmail.com>
2022-04-21 20:14:07 +0200

    Video styling done

--- pages/skapaannons/index.js
+++ pages/skapaannons/index.js
@@ -11,29 +11,34 @@ function SkapaAnnons() {
       <div>
         <div className="container mx-auto mt-[50px] flex items-center justify-center bg-white">
           <div className="w-[548px]">
-            <h1 className="mb-[24px] text-[36px] text-indigo-800">
-              Hej Marianne! <br />
-              Det är enkelt att bli hyresvärd.
-            </h1>
-            <p className="mb-[40px] text-[24px]">
-              Till höger ser du en video som förklarar hur det går till. När du
-              är redo klickar du på sätt igång!
-            </p>
+            <div className="mr-[20px]">
+              <h1 className="mb-[24px] text-[36px] text-indigo-800">
+                Hej Marianne! <br />
+                Det är enkelt att bli hyresvärd.
+              </h1>
+              <p className="mb-[40px] text-[24px]">
+                Till höger ser du en video som förklarar hur det går till. När
+                du är redo klickar du på sätt igång!
+              </p>
+            </div>
             <Link href="/skapaannons/1">
               <button className="h-[72px] w-[454px] rounded-full bg-indigo-500 text-[24px] text-white">
                 Sätt igång
               </button>
             </Link>
           </div>
-          <div className="relative overflow-hidden">
-            <Image
-              className="h-full w-full"
-              src="/video.png"
-              alt="test"
-              width="548"
-              height="574"
-            ></Image>
-          </div>
+          <Link href="/skapaannons/video">
+            <div className="flex max-h-[574px] max-w-[548px]">
+              <img
+                className="rounded-3xl object-cover"
+                src="/video.png"
+                alt="Linköping"
+              />
+              <button className="absolute h-[32px] w-[124px] translate-y-[250px] translate-x-[200px]">
+                <Image src="/play-button.svg" width="77" height="99" />
+              </button>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
--- pages/skapaannons/video.tsx
+++ pages/skapaannons/video.tsx
@@ -1,14 +1,61 @@
 import React from 'react'
+import ButtonLg from '../../components/Buttons/ButtonLg'
 import LoginHeader from '../../components/LoginHeader'
+import Link from 'next/link'
+import Image from 'next/image'
 import Video from '../../components/Video'
-function SkapaAnnons1() {
+
+function SkapaAnnons() {
   return (
     <div>
-      <LoginHeader add="Spara och " link="/profil" />
-      <p>SE VIDEO</p>
-      <Video />
+      <div className="absolute z-10 flex h-screen w-screen items-center justify-center bg-black bg-opacity-80">
+        <div>
+          <Link href="/skapaannons">
+            <p className="mb-2 text-right text-lg text-white hover:cursor-pointer hover:text-indigo-500">
+              Stäng video
+            </p>
+          </Link>
+          <Video />
+        </div>
+      </div>
+      <div>
+        <LoginHeader link="/profil" />
+        <div>
+          <div className="container mx-auto mt-[50px] flex items-center justify-center bg-white">
+            <div className="w-[548px]">
+              <div className="mr-[20px]">
+                <h1 className="mb-[24px] text-[36px] text-indigo-800">
+                  Hej Marianne! <br />
+                  Det är enkelt att bli hyresvärd.
+                </h1>
+                <p className="mb-[40px] text-[24px]">
+                  Till höger ser du en video som förklarar hur det går till. När
+                  du är redo klickar du på sätt igång!
+                </p>
+              </div>
+              <Link href="/skapaannons/1">
+                <button className="h-[72px] w-[454px] rounded-full bg-indigo-500 text-[24px] text-white">
+                  Sätt igång
+                </button>
+              </Link>
+            </div>
+            <Link href="/skapaannons/video">
+              <div className="flex max-h-[574px] max-w-[548px]">
+                <img
+                  className="rounded-3xl object-cover"
+                  src="/video.png"
+                  alt="Linköping"
+                />
+                <button className="absolute h-[32px] w-[124px] translate-y-[250px] translate-x-[200px]">
+                  <Image src="/play-button.svg" width="77" height="99" />
+                </button>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default SkapaAnnons1
+export default SkapaAnnons
